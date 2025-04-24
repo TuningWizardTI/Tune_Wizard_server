742c3eeba5d57b49c8a90f4f767dc4da72afa610
wogud556 <wogud556@naver.com>
2025-04-24 14:19:22 +0900

    start

---
+++ src/main/java/com/hanati/cop/tuneWizard/TuneWizardApplication.java
@@ -0,0 +1,13 @@
+package com.hanati.cop.tuneWizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneWizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanati/cop/tuneWizard/TuneWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanati.cop.tuneWizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
